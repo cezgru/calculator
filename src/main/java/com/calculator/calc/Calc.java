9eeed040856953e481009ff30c0a457f48f2db8f
Cezary Gruszka <cezgru@gmail.com>
2018-11-20 07:18:35 +0100

    seriously?

---
+++ src/main/java/com/calculator/calc/Calc.java
@@ -0,0 +1,19 @@
+package com.calculator.calc;
+
+public class Calc {
+    public double add(double a, double b){
+        return a+b;
+    }
+
+    public double subtract(double a, double b){
+        return a-b;
+    }
+
+    public double multiply(double a, double b){
+        return a*b;
+    }
+
+    public double divide(double a, double b){
+        return a/b;
+    }
+}
